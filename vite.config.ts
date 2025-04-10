55d53b9716430496c9ecf397206c8fe9784c5134
Matt B <9885259+Mattamorphic@users.noreply.github.com>
2025-04-10 10:57:40 +0100

    fix issue with cors

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import markdown from "vite-plugin-markdown";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import markdown from 'vite-plugin-markdown';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), markdown()], // Add markdown plugin to the plugins array
-  server: {
-    open: true, // Automatically open the browser on server start
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
+  base: '/mattamorphic.github.io/',
+  plugins: [
+    react(),
+    markdown(), // Add Markdown plugin to handle .md files
+  ],
 });
