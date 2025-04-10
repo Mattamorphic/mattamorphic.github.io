9dfc14fd95de38bab21ed36229679cbb8adb8a0e
Matt B <9885259+Mattamorphic@users.noreply.github.com>
2025-04-10 11:18:18 +0100

    still fiddling with cors errors

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,23 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import markdown from 'vite-plugin-markdown';
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/mattamorphic.github.io/' : '/',
+export default defineConfig(() => ({
   plugins: [
     react(),
     markdown(),
   ],
   server: {
     open: true, // Automatically open the app in the browser during development
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Allow cross-origin requests
+    },
+    proxy: {
+      '/assets': {
+        target: 'https://mattamorphic.github.io', // Replace with the actual server hosting the assets
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/assets/, ''),
+      },
+    },
   },
   build: {
     rollupOptions: {
