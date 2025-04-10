d44a43c4565d2b27c757e73442eb49abdcffca92
Matt B <9885259+Mattamorphic@users.noreply.github.com>
2025-04-10 11:06:51 +0100

    build in production mode

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import markdown from 'vite-plugin-markdown';
 
-export default defineConfig({
-  base: '/mattamorphic.github.io/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/mattamorphic.github.io/' : '/',
   plugins: [
     react(),
-    markdown(), // Add Markdown plugin to handle .md files
+    markdown(),
   ],
-});
+}));
